4ab27287e90404beafff0a73b1517c4c0037516e
Mayank Verma <mayankverma.dev@gmail.com>
2025-05-07 15:38:47 +0530

    trying to get stashed code

--- src/Component/Carrousal/Carrousal.jsx
+++ src/Component/Carrousal/Carrousal.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { useKeenSlider } from "keen-slider/react"
 import "keen-slider/keen-slider.min.css"
-import "./styles.css"
+import "./Cstyles.css"
 
 export default function Carrousal() {
   const [sliderRef] = useKeenSlider({
@@ -10,8 +10,8 @@ export default function Carrousal() {
 
   return (
     <div ref={sliderRef} className="keen-slider"> 
-      <div className="keen-slider__slide number-slide1"><img src="./1.png" alt="" /></div>
-      <div className="keen-slider__slide number-slide2">2</div>
+      <div className="keen-slider__slide number-slide1"><img src="./c1.jpeg" alt="" /></div>
+      <div className="keen-slider__slide number-slide2"><img src="./c2.jpg" alt="" /></div>
       <div className="keen-slider__slide number-slide3">3</div>
       <div className="keen-slider__slide number-slide4">4</div>
       <div className="keen-slider__slide number-slide5">5</div>
